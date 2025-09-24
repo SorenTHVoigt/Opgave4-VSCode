9f272016bc683f07b4a9d4cd5435b2d045715e90
SorenTHVoigt <soren@voigt.nu>
2025-09-24 13:49:38 +0200

    opdate fra IntelliJ

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from VSCode!");
-      System.out.println("And hello from IntelliJ");
+      System.out.println("And hello from IntelliJ- Konflikt i fra IntelliJ");
     }
 }
